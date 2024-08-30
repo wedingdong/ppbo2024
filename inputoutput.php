8dcc6ec95fc708ad4e7b569d0dd14052fa6a06e7
lzzfadlin <h1101231012@student.untan.ac.id>
2024-08-30 15:08:19 +0700

    Praktikum 2b - Menggunakan Fungsi Bawaan

--- inputoutput.php
+++ inputoutput.php
@@ -1,3 +1,14 @@
 <?php
+date_default_timezone_set("Asia/Jakarta");
 $nama = readline('Masukkan nama Anda: ');
-echo "Halo {$nama}";
+$waktu = date('H:i'); // Menambahkan menit
+
+if (date('H') >= 5 && date('H') < 11) {
+    echo "Selamat Pagi, {$nama}, sekarang pukul {$waktu}\n";
+} elseif (date('H') >= 11 && date('H') < 15) {
+    echo "Selamat Siang, {$nama}, sekarang pukul {$waktu}\n";
+} elseif (date('H') >= 15 && date('H') < 19) {
+    echo "Selamat Sore, {$nama}, sekarang pukul {$waktu}\n";
+} else {
+    echo "Selamat Malam, {$nama}, sekarang pukul {$waktu}\n";
+}
