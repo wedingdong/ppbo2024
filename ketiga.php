3f9fe71fb0031c2b21e803d0c371c54ef364f85a
lzzfadlin <h1101231012@student.untan.ac.id>
2024-08-30 15:38:38 +0700

    Praktikum 4C - Membuat Constructor

--- ketiga.php
+++ ketiga.php
@@ -5,6 +5,10 @@ class Lingkaran
     const PHI = 3.14;
     public $jari_jari;
 
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function luas() : float {
         return self::PHI*$this->jari_jari*$this->jari_jari;
     }
@@ -19,6 +23,10 @@ class Bola
     const PHI = 3.14;
     public $jari_jari;
 
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function volume() : float {
         return (4/3)*self::PHI*pow($this->jari_jari,3);
     }
@@ -29,6 +37,11 @@ class Tabung
     const PHI = 3.14;
     public $jari_jari;
     public $tinggi;
+
+    public function __construct($jari_jari, $tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
 
     public function volume() : float {
         return self::PHI*pow($this->jari_jari,2)*$this->tinggi;
@@ -41,32 +54,27 @@ class Kerucut
     public $jari_jari;
     public $tinggi;
 
+    public function __construct($jari_jari, $tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
     public function volume() : float {
         return (1/3)*self::PHI*pow($this->jari_jari,2)*$this->tinggi;
     }
 }
 
-// Implementasi class Lingkaran
-$lingkaran = new Lingkaran();
-$lingkaran->jari_jari = 5;
+$lingkaran = new Lingkaran(5);
 echo "Luas Lingkaran: " . $lingkaran->luas() . "<br>";
 echo "Keliling Lingkaran: " . $lingkaran->keliling() . "<br>";
 
-// Implementasi class Bola
-$bola = new Bola();
-$bola->jari_jari = 5;
+$bola = new Bola(5);
 echo "Volume Bola: " . $bola->volume() . "<br>";
 
-// Implementasi class Tabung
-$tabung = new Tabung();
-$tabung->jari_jari = 5;
-$tabung->tinggi = 10;
+$tabung = new Tabung(5, 10);
 echo "Volume Tabung: " . $tabung->volume() . "<br>";
 
-// Implementasi class Kerucut
-$kerucut = new Kerucut();
-$kerucut->jari_jari = 5;
-$kerucut->tinggi = 10;
+$kerucut = new Kerucut(5, 10);
 echo "Volume Kerucut: " . $kerucut->volume() . "<br>";
 
 ?>
