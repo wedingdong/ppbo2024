7bf936001218a7accd3a764712826b02765d9892
lzzfadlin <h1101231012@student.untan.ac.id>
2024-08-30 15:36:14 +0700

    Praktikum 4B - Membuat Atribut

--- ketiga.php
+++ ketiga.php
@@ -3,51 +3,70 @@
 class Lingkaran
 {
     const PHI = 3.14;
+    public $jari_jari;
 
-    public function luas($jari_jari) : float {
-        return self::PHI * $jari_jari * $jari_jari;
+    public function luas() : float {
+        return self::PHI*$this->jari_jari*$this->jari_jari;
     }
 
-    public function keliling($jari_jari) : float {
-        return 2 * self::PHI * $jari_jari;
+    public function keliling() : float {
+        return 2*self::PHI*$this->jari_jari;
     }
 }
 
 class Bola
 {
     const PHI = 3.14;
+    public $jari_jari;
 
-    public function volume($jari_jari) : float {
-        return (4 / 3) * self::PHI * pow($jari_jari, 3);
+    public function volume() : float {
+        return (4/3)*self::PHI*pow($this->jari_jari,3);
     }
 }
 
 class Tabung
 {
     const PHI = 3.14;
+    public $jari_jari;
+    public $tinggi;
 
-    public function volume($jari_jari, $tinggi) : float {
-        return self::PHI * pow($jari_jari, 2) * $tinggi;
+    public function volume() : float {
+        return self::PHI*pow($this->jari_jari,2)*$this->tinggi;
     }
 }
 
 class Kerucut
 {
     const PHI = 3.14;
+    public $jari_jari;
+    public $tinggi;
 
-    public function volume($jari_jari, $tinggi) : float {
-        return (1 / 3) * self::PHI * pow($jari_jari, 2) * $tinggi;
+    public function volume() : float {
+        return (1/3)*self::PHI*pow($this->jari_jari,2)*$this->tinggi;
     }
 }
 
-// Membuat objek dari class Kerucut
-$nasi_tumpeng = new Kerucut();
+// Implementasi class Lingkaran
+$lingkaran = new Lingkaran();
+$lingkaran->jari_jari = 5;
+echo "Luas Lingkaran: " . $lingkaran->luas() . "<br>";
+echo "Keliling Lingkaran: " . $lingkaran->keliling() . "<br>";
 
-// Menghitung volume dan menampilkan hasilnya
-$jari_jari = 4;  // dalam cm
-$tinggi = 10;    // dalam cm
-$volume = $nasi_tumpeng->volume($jari_jari, $tinggi);
+// Implementasi class Bola
+$bola = new Bola();
+$bola->jari_jari = 5;
+echo "Volume Bola: " . $bola->volume() . "<br>";
 
-echo "Volume kerucut dengan jari-jari $jari_jari cm dan tinggi $tinggi cm adalah $volume cm³.";
+// Implementasi class Tabung
+$tabung = new Tabung();
+$tabung->jari_jari = 5;
+$tabung->tinggi = 10;
+echo "Volume Tabung: " . $tabung->volume() . "<br>";
+
+// Implementasi class Kerucut
+$kerucut = new Kerucut();
+$kerucut->jari_jari = 5;
+$kerucut->tinggi = 10;
+echo "Volume Kerucut: " . $kerucut->volume() . "<br>";
 
 ?>
