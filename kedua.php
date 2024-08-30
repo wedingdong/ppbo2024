dc1c46756c2c0cdfb83d8d026ae962e89ceaaafc
lzzfadlin <h1101231012@student.untan.ac.id>
2024-08-30 15:24:35 +0700

    Praktikum 2C - Function Sederhana

--- kedua.php
+++ kedua.php
@@ -1,9 +1,33 @@
 <?php
 
 function luasLingkaran($jari) : float {
-    $luas = 3.14*$jari*$jari;
-    return $luas;
+    return 3.14 * $jari * $jari;
 }
 
-$luas_tanah = luasLingkaran(45);
-echo "Luas tanah budi adalah {$luas_tanah}";
+function kelilingLingkaran($jari) : float {
+    return 2 * 3.14 * $jari;
+}
+
+function volumeBola($jari) : float {
+    return (4/3) * 3.14 * pow($jari, 3);
+}
+
+function volumeTabung($jari, $tinggi) : float {
+    return 3.14 * pow($jari, 2) * $tinggi;
+}
+
+function volumeKerucut($jari, $tinggi) : float {
+    return (1/3) * 3.14 * pow($jari, 2) * $tinggi;
+}
+
+// Contoh Penggunaan Fungsi
+$jari = 45;
+$tinggi = 100;
+
+echo "Luas lingkaran dengan jari-jari $jari adalah " . luasLingkaran($jari) . "\n";
+echo "Keliling lingkaran dengan jari-jari $jari adalah " . kelilingLingkaran($jari) . "\n";
+echo "Volume bola dengan jari-jari $jari adalah " . volumeBola($jari) . "\n";
+echo "Volume tabung dengan jari-jari $jari dan tinggi $tinggi adalah " . volumeTabung($jari, $tinggi) . "\n";
+echo "Volume kerucut dengan jari-jari $jari dan tinggi $tinggi adalah " . volumeKerucut($jari, $tinggi) . "\n";
+
+?>
