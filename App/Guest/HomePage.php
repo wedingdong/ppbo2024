3ffb8270c3715d847e1fb7dec4f5158f157ba375
lzzfadlin <h1101231012@student.untan.ac.id>
2024-10-05 21:35:49 +0700

    6Bx - Merapikan Lokasi Kode Program

--- App/Guest/HomePage.php
+++ App/Guest/HomePage.php
@@ -6,3 +6,23 @@ class HomePage
         echo "Halaman depan guest";
     }
 }
+
+namespace App\Admin;
+class HomePage
+{
+    public function tampil() : void {
+        echo "Halaman depan admin";
+    }
+}
+
+namespace App\Admin;
+class ManajemenArtikel
+{
+    public function tambah() : void
+    {
+        echo "Artikel berhasil ditambahkan";
+    }
+}
+
+$hp = new \App\Guest\HomePage();
+$hp->tampil();
